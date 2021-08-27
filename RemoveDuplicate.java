77822f20c41bccd787131c6d87d19ebce2c34795
Aswini G <gaswini313@gmail.com>
2021-08-27 14:31:37 +0530

    Add files via upload

---
+++ RemoveDuplicate.java
@@ -0,0 +1,38 @@
+package week3.day2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class RemoveDuplicate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String text= "PayPal India";
+		String text1=text.toLowerCase();
+		String text2=text1.replaceAll(" ","");
+		char ch[]=text2.toCharArray();
+		Set<Character> CharSet=new LinkedHashSet<Character>();
+		Set<Character> DupCharSet=new LinkedHashSet<Character>();
+		for(char c:ch)
+		{
+			boolean add=CharSet.add(c);
+			if(add==false)
+			{
+				DupCharSet.add(c);
+			}
+		}
+		System.out.println("Duplicate character:" + DupCharSet);
+		CharSet.removeAll(DupCharSet);
+		System.out.println("Character:" + CharSet);
+		for(Character str:CharSet)
+		{
+			System.out.println(str);
+		}
+		
+
+	}
+
+}
---
+++ SecondLargest.java
@@ -0,0 +1,27 @@
+package week3.day2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SecondLargest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] data = {3,2,11,4,6,7};
+		Arrays.sort(data);
+		List<Integer> list=new ArrayList<Integer>();
+		for(Integer arr1:data)
+		{
+			list.add(arr1);
+		}
+		System.out.println("Sorted ArrayList:" + list);
+		
+		int large=list.get(list.size()-2);
+		System.out.println("SecondLargest:" + large);
+	}
+
+	
+
+}
